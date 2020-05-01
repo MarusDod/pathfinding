7e220546bc5b55686c881b0027554bb36213266f
MarusDod <64314098+MarusDod@users.noreply.github.com>
2020-05-01 19:35:10 +0100

    Path finding
    Using A* star to find out the shortest path between 2 nodes in a grid

---
+++ sketch.js
@@ -0,0 +1,166 @@
+var list;
+var openlist;
+var closedlist;
+var obstaclelist;
+var pathlist;
+var inicio;
+var current;
+var finish;
+var frameX = 500;
+var frameY = 500;
+var rows = 30;
+var cols = 20;
+var coolMouse;
+var running;
+var started;
+var pause;
+var button1;
+var button2;
+var boolnode;
+var buttonPause;
+
+function setup(){
+  createCanvas(frameX+100,frameY+50);
+  boolnode=0;
+  coolMouse=0;
+  started=false;
+  running=true;
+  pause=false;
+  textSize(27);
+  button1 = createButton('Start');
+  buttonPause = createButton('Pause');
+  button2 = createButton('End');
+  button1.position(frameX,0);
+  buttonPause.position(frameX,50);
+  button2.position(frameX,100);
+  button1.style('font-size','40px');
+  buttonPause.style('font-size','40px');
+  button2.style('font-size','40px');
+  button1.mousePressed(getStarted);
+  buttonPause.mousePressed(pausa);
+  button2.mousePressed(setup);
+  list = new Array();
+    openlist=new Array();
+    closedlist=new Array();
+    obstaclelist=new Array();
+    pathlist = new Array();
+    for(let i=0;i<rows;i++){
+      list.push(new Array());
+      for(let j=0;j<cols;j++){
+          list[i].push(new Spot(i,j));
+      }
+    }
+
+}
+
+function draw(){
+  background(220);
+if(pause==false){
+if(started==true){
+  if(boolnode==2){
+  if(list[0][0].f==0){
+  inicio.setDistance(inicio);
+  openlist.push(inicio);
+  boolnode=3;
+  }
+}
+if(openlist.length>0 && running==true){
+  removefromarray(openlist,current);
+  if(!closedlist.includes(current)) closedlist.push(current);
+  current.addNeighbours();
+  if(openlist.length>0) current=current.lowestF();
+  if(current==finish) running=false;
+}
+else if(running==false && openlist.length>0){
+  if(!pathlist.includes(current)){
+     pathlist.push(current);
+     while(current.parent!==undefined){
+       current=current.parent;
+       pathlist.push(current);
+     }
+    }
+  }
+}
+else{
+  if(boolnode<=2 && (millis()-coolMouse>100 || boolnode==2)){
+    coolMouse=millis();
+    actionMouse();
+  }
+}
+}
+  for(var i=0;i<rows;i++){
+    for(var j=0;j<cols;j++){
+        list[i][j].display();
+    }
+  }
+  fill(200,200,15);
+  rect(0,frameY,100,50);
+  text(" - Shortest Path",100,frameY+35)
+  fill(0,0,300);
+  rect(300,frameY,100,50);
+  text(" - Closed List",400,frameY+35)
+  line(0,frameY,frameX,frameY);
+  line(frameX,0,frameX,frameY);
+}
+
+
+function rand(min,max){
+  return parseInt((Math.random()*(max-min))+min);
+}
+
+function maxi(num1,num2){
+  return num1>num2?num1:num2;
+}
+
+function mini(num1,num2){
+  return num1<num2?num1:num2;
+}
+
+function removefromarray(array,object){
+  if(array.includes(object)){
+  for(var i=0;i<array.length;i++){
+    if(array[i]==object){
+      array.splice(i,1);
+      break;
+    }
+  }
+}
+}
+function getStarted(){
+  started=true;
+}
+function setBool(i,j){
+  if(boolnode==0){
+    inicio=list[i][j];
+    current=inicio;
+    boolnode++;
+  }
+  else if(boolnode==1){
+    finish=list[i][j];
+    boolnode++;
+  }
+  else{
+    if(!obstaclelist.includes(list[i][j]) && list[i][j]!=finish) obstaclelist.push(list[i][j]);
+  }
+}
+function actionMouse(){
+    if(mouseIsPressed){
+    for(var i=0;i<rows;i++){
+      for(var j=0;j<cols;j++){
+          if(collidePointRect(mouseX,mouseY,list[i][j].posX,list[i][j].posY,list[i][j].width,list[i][j].height)){
+              setBool(i,j);
+          }
+        }
+      }
+    }
+}
+function pausa(){
+  if(pause==true) pause=false;
+  else pause=true;
+}
+function delay(time){
+  let cooldown=0;
+  while(millis()-cooldown>time){
+    cooldown=millis();
+  }
+}
---
+++ spot.js
@@ -0,0 +1,87 @@
+function Spot(i,j){
+  this.i=i;
+  this.j=j;
+  this.posX=this.j*(frameX/cols);
+  this.posY=this.i*(frameY/rows);
+  this.width=frameX/cols;
+  this.height=frameY/rows;
+  this.f=0;
+  this.g=0;
+  this.h=0;
+  this.parent=undefined;
+
+this.setDistance = function(node){
+    this.g=node.g+this.computeDistance(node);
+    this.h=this.computeDistance(finish);
+    this.f=this.g+this.h;
+}
+this.computeDistance = function(node){
+  return (abs(this.i-(node.i))*10)+(abs(this.j-(node.j))*10);
+}
+
+this.addNeighbours = function(){
+    for(var i=-1;i<=1;i++){
+      for(var j=-1;j<=1;j++){
+        if((abs(j)!=abs(i)) && this.i+i>=0 && this.j+j>=0 && this.i+i<rows && this.j+j<cols){
+            if(!closedlist.includes(list[this.i+i][this.j+j]) && !obstaclelist.includes(list[this.i+i][this.j+j])){
+              if(!openlist.includes(list[this.i+i][this.j+j])){
+                openlist.push(list[this.i+i][this.j+j]);
+              }
+                list[this.i+i][this.j+j].updateParent();
+            }
+          }
+        }
+    }
+}
+
+this.lowestF = function(){
+    let minor=openlist[0].f;
+    var index=0;
+    for(var i=1;i<openlist.length;i++){
+      openlist[i].setDistance(this);
+      if(openlist[i].f<minor && !closedlist.includes(openlist[i]) && !obstaclelist.includes(openlist[i])){
+        minor=openlist[i].f;
+        index=i;
+      }
+    }
+    return openlist[index];
+}
+
+this.updateParent = function(){
+  let obj=undefined;
+  let minorG=rows*cols*10
+  for(var i=-1;i<=1;i++){
+    for(var j=-1;j<=1;j++){
+      if((abs(j)!=abs(i)) && this.i+i>=0 && this.j+j>=0 && this.i+i<rows && this.j+j<cols){
+          if(closedlist.includes(list[this.i+i][this.j+j]) && list[this.i+i][this.j+j].g<minorG){
+            obj=list[this.i+i][this.j+j];
+            minorG=obj.g;
+          }
+      }
+    }
+  }
+  this.parent=obj;
+}
+
+this.display = function(){
+    if(this==finish) fill(0,0,200);
+    else if(inicio==this) fill(0,150,200);
+    else if(current==this)   fill(0,0,0);
+    else{
+      if(pathlist.includes(this)){
+        fill(200,200,15);
+      }
+      else if(closedlist.includes(this)){
+        fill(0,0,300);
+      }
+      else if(openlist.includes(this)){
+        fill(300,100,100);
+      }
+      else if(obstaclelist.includes(this)){
+        fill(20,80,20);
+      }
+      else fill(0,200,0);
+    }
+    rect(this.posX,this.posY,this.width,this.height);
+  }
+}
